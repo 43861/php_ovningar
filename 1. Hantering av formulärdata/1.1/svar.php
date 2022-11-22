9ac7178e4344b6facf22d9aaa30361ef40d9a558
DKertuser <danielkertsmik@gmail.com>
2022-11-22 10:56:04 +0200

    Added 1.1 php

---
+++ 1. Hantering av formulärdata/1.1/svar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: gray; color: white;">
+    
+
+
+<?php
+
+echo "<h2> Din nanmn: " . $_GET['name'] . "<br> </h2> ";
+
+echo "<h2> Din efternamn: " . $_GET['familyName'] . "<br> </h2> ";
+
+echo "<h2> Din address: " . $_GET['address'] . "<br> </h2> ";
+
+echo "<h2> Din telefonnummer: " . $_GET['phone'] . "<br> </h2> ";
+
+?>
+
+</body>
+</html>
